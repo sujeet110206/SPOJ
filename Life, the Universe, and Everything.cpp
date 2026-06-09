b020f8d7629842893be5806a9dcbbd8789970fb4
Sujeet Kumar <sujeet901626@gmail.com>
2026-06-09 10:15:05 +0530

    Create Life, the Universe, and Everything.cpp

---
+++ Life, the Universe, and Everything.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define sp ' '
+#define pb push_back
+#define inf 1e18
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+void solve() {
+  while (true) {
+    int n;
+    cin >> n;
+    if (n == 42) break;
+    else cout << n << endl;
+  }
+}
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  solve();
+  return 0;
+}
